d810261adec0734691339c555d3f2f9c7c089d9d
sakiraahammed <sakira.ahammed@gmail.com>
2025-08-07 22:40:58 +0600

    3rd Commit

--- Group42_DESCO/src/main/java/com/example/group42_desco/Sakira/MDDashBoardController.java
+++ Group42_DESCO/src/main/java/com/example/group42_desco/Sakira/MDDashBoardController.java
@@ -1,6 +1,11 @@
 package com.example.group42_desco.Sakira;
 
+import com.example.group42_desco.HelloApplication;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class MDDashBoardController
 {
@@ -10,15 +15,35 @@ public class MDDashBoardController
 
     @javafx.fxml.FXML
     public void ApprovalOfSalarySheetOA(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Sakira/ApprovalOfSalarySheet.fxml"));
+            Scene nextScene = new Scene(fxmlLoader.load());
+            Stage nextStage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+            nextStage.setTitle("Approval Of Salary Sheet");
+            nextStage.setScene(nextScene);
+            nextStage.show();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @javafx.fxml.FXML
     public void SetUpTandCOA(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Sakira/SetTandC.fxml"));
+            Scene nextScene = new Scene(fxmlLoader.load());
+            Stage nextStage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+            nextStage.setTitle("Set T&C");
+            nextStage.setScene(nextScene);
+            nextStage.show();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
-    @javafx.fxml.FXML
-    public void LoadOA(ActionEvent actionEvent) {
-    }
+
 
     @javafx.fxml.FXML
     public void ViewFeedbackOA(ActionEvent actionEvent) {
--- Group42_DESCO/src/main/java/module-info.java
+++ Group42_DESCO/src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.group42_desco {
 
 
     opens com.example.group42_desco to javafx.fxml;
+    opens com.example.group42_desco.Sakira to javafx.fxml;
     exports com.example.group42_desco;
     exports com.example.group42_desco.Nipa2211056;
     opens com.example.group42_desco.Nipa2211056 to javafx.fxml;
