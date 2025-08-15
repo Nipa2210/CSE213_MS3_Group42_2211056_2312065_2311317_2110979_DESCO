5556fd953168aeed2b91ae67d151fb40888e7199
sakiraahammed <sakira.ahammed@gmail.com>
2025-08-15 20:58:31 +0600

    11th Commit

--- Group42_DESCO/src/main/java/com/example/group42_desco/Sakira/ViewElectricityStorageController.java
+++ Group42_DESCO/src/main/java/com/example/group42_desco/Sakira/ViewElectricityStorageController.java
@@ -4,37 +4,40 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class ViewElectricityStorageController
 {
-    @javafx.fxml.FXML
-    private TableColumn StatusOA;
     @javafx.fxml.FXML
     private TableColumn ItemNameTC;
     @javafx.fxml.FXML
     private TableColumn QuantityTC;
     @javafx.fxml.FXML
-    private ComboBox FilterCB;
-    @javafx.fxml.FXML
     private TableView ViewElectricityStorageTV;
     @javafx.fxml.FXML
     private TableColumn LocationTC;
+    @javafx.fxml.FXML
+    private TextField StatusTF;
+    @javafx.fxml.FXML
+    private TableColumn StatusTC;
+    @javafx.fxml.FXML
+    private TextField ItemNameTF;
+    @javafx.fxml.FXML
+    private TextField QuantityTF;
+    @javafx.fxml.FXML
+    private TextField LocationTF;
 
     @javafx.fxml.FXML
     public void initialize() {
     }
 
-    @javafx.fxml.FXML
-    public void InventoryOA(ActionEvent actionEvent) {
-    }
+
 
     @javafx.fxml.FXML
     public void PreviousOA(ActionEvent actionEvent) {
     }
 
-    @javafx.fxml.FXML
-    public void ExportOA(ActionEvent actionEvent) {
-    }
+
 
     @javafx.fxml.FXML
     public void UpdatesandDisplaysFilteredResultsOA(ActionEvent actionEvent) {
